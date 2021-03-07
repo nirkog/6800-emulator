814917d91cc4e08fa0a9dd142d992f4036f8322c
Nir Kogman <nirkog@gmail.com>
2021-03-08 01:41:38 +0200

    Added support for 10 more opcodes

--- src/disassembler/disassembler.rs
+++ src/disassembler/disassembler.rs
@@ -2,6 +2,16 @@
 #[derive(Debug, PartialEq, Eq)]
 pub enum Opcode {
     Nop,
+    TransferAToConditionCodes,
+    TransferConditionCodesToA,
+    IncrementIndexRegister,
+    DecrementIndexRegister,
+    ClearOverflowFlag,
+    SetOverflowFlag,
+    ClearCarryFlag,
+    SetCarryFlag,
+    ClearInterruptMask,
+    SetInterruptMask,
     SubstractAImmediate,
     SubstractADirect,
     SubstractAIndexed,
@@ -15,13 +25,19 @@ pub enum OperandType {
     AccumulatorB,
     IndexRegister,
     Immediate8,
-    Immediate16
+    Immediate16,
+    ConditionCodeRegister
 }
 
 /// Opcode groups that group together opcodes with different codes but similar meaning
 #[derive(Debug, PartialEq, Eq)]
 pub enum OpcodeGroup {
     Substract,
+    TransferRegisters,
+    IncrementRegister,
+    DecrementRegister,
+    ClearFlag,
+    SetFlag,
     NoGroup
 }
 
@@ -60,6 +76,16 @@ pub enum DisassemblyError {
 fn match_byte_to_opcode_info(byte: u8) -> Result<OpcodeInfo, DisassemblyError> {
     let opcode_info: OpcodeInfo = match byte {
         0x01 => OpcodeInfo { opcode: Opcode::Nop, group: OpcodeGroup::NoGroup, instruction_length: 1, cycles: 1 },
+        0x06 => OpcodeInfo { opcode: Opcode::TransferAToConditionCodes, group: OpcodeGroup::TransferRegisters, instruction_length: 1, cycles: 2},
+        0x07 => OpcodeInfo { opcode: Opcode::TransferConditionCodesToA, group: OpcodeGroup::TransferRegisters, instruction_length: 1, cycles: 2},
+        0x08 => OpcodeInfo { opcode: Opcode::IncrementIndexRegister, group: OpcodeGroup::IncrementRegister, instruction_length: 1, cycles: 4},
+        0x09 => OpcodeInfo { opcode: Opcode::DecrementIndexRegister, group: OpcodeGroup::DecrementRegister, instruction_length: 1, cycles: 4},
+        0x0A => OpcodeInfo { opcode: Opcode::ClearOverflowFlag, group: OpcodeGroup::ClearFlag, instruction_length: 1, cycles: 2},
+        0x0B => OpcodeInfo { opcode: Opcode::SetOverflowFlag, group: OpcodeGroup::SetFlag, instruction_length: 1, cycles: 2},
+        0x0C => OpcodeInfo { opcode: Opcode::ClearCarryFlag, group: OpcodeGroup::ClearFlag, instruction_length: 1, cycles: 2},
+        0x0D => OpcodeInfo { opcode: Opcode::SetCarryFlag, group: OpcodeGroup::SetFlag, instruction_length: 1, cycles: 2},
+        0x0E => OpcodeInfo { opcode: Opcode::ClearInterruptMask, group: OpcodeGroup::ClearFlag, instruction_length: 1, cycles: 2},
+        0x0F => OpcodeInfo { opcode: Opcode::SetInterruptMask, group: OpcodeGroup::SetFlag, instruction_length: 1, cycles: 2},
         0x80 => OpcodeInfo { opcode: Opcode::SubstractAImmediate, group: OpcodeGroup::Substract, instruction_length: 2, cycles: 2 },
         0x90 => OpcodeInfo { opcode: Opcode::SubstractADirect, group: OpcodeGroup::Substract, instruction_length: 2, cycles: 3 },
         0xA0 => OpcodeInfo { opcode: Opcode::SubstractAIndexed, group: OpcodeGroup::Substract, instruction_length: 2, cycles: 5 },
@@ -73,6 +99,16 @@ fn match_byte_to_opcode_info(byte: u8) -> Result<OpcodeInfo, DisassemblyError> {
 fn disassemble_operands(opcode_info: &OpcodeInfo, data: &[u8]) -> Vec<OperandInfo> {
     let operands = match opcode_info.opcode {
         Opcode::Nop => vec![],
+        Opcode::TransferAToConditionCodes => vec![OperandInfo { operand_type: OperandType::AccumulatorA, operand_value: None }, OperandInfo { operand_type: OperandType::ConditionCodeRegister, operand_value: None }],
+        Opcode::TransferConditionCodesToA => vec![OperandInfo { operand_type: OperandType::ConditionCodeRegister, operand_value: None }, OperandInfo { operand_type: OperandType::AccumulatorA, operand_value: None }],
+        Opcode::IncrementIndexRegister => vec![OperandInfo { operand_type: OperandType::IndexRegister, operand_value: None }],
+        Opcode::DecrementIndexRegister => vec![OperandInfo { operand_type: OperandType::IndexRegister, operand_value: None }],
+        Opcode::ClearOverflowFlag => vec![OperandInfo { operand_type: OperandType::ConditionCodeRegister, operand_value: None }],
+        Opcode::SetOverflowFlag => vec![OperandInfo { operand_type: OperandType::ConditionCodeRegister, operand_value: None }],
+        Opcode::ClearCarryFlag => vec![OperandInfo { operand_type: OperandType::ConditionCodeRegister, operand_value: None }],
+        Opcode::SetCarryFlag => vec![OperandInfo { operand_type: OperandType::ConditionCodeRegister, operand_value: None }],
+        Opcode::ClearInterruptMask => vec![OperandInfo { operand_type: OperandType::ConditionCodeRegister, operand_value: None }],
+        Opcode::SetInterruptMask => vec![OperandInfo { operand_type: OperandType::ConditionCodeRegister, operand_value: None }],
         Opcode::SubstractAImmediate => vec![OperandInfo { operand_type: OperandType::AccumulatorA, operand_value: None }, OperandInfo { operand_type: OperandType::Immediate8, operand_value: Some(data[1] as u16) }],
         Opcode::SubstractADirect => vec![OperandInfo { operand_type: OperandType::AccumulatorA, operand_value: None }, OperandInfo { operand_type: OperandType::Immediate8, operand_value: Some(data[1] as u16) }],
         Opcode::SubstractAIndexed => vec![OperandInfo { operand_type: OperandType::AccumulatorA, operand_value: None }, OperandInfo { operand_type: OperandType::Immediate8, operand_value: Some(data[1] as u16) }, OperandInfo { operand_type: OperandType::IndexRegister, operand_value: None }],
