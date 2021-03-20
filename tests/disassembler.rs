b3145e4e05d0eb477c813963cafac4324a65cafd
Nir Kogman <nirkog@gmail.com>
2021-03-20 15:06:29 +0200

    Updated test

--- tests/disassembler.rs
+++ tests/disassembler.rs
@@ -6,6 +6,7 @@ mod disassembler_tests {
     fn test_substract_immediate8_instruction() {
         let sub_a_immediate: [u8; 2] = [0x80, 0x5];  
         let sub_a_immediate_disassembly = disassemble_instruction(&sub_a_immediate);
+        let sub_a_operands: Vec<OperandType>;
 
         // Make sure there were no errors
         assert_eq!(sub_a_immediate_disassembly.is_err(), false);
@@ -16,9 +17,10 @@ mod disassembler_tests {
         assert_eq!(sub_a_immediate_disassembly.opcode_info.group, OpcodeGroup::Substract);
         assert_eq!(sub_a_immediate_disassembly.opcode_info.instruction_length, 2);
         assert_eq!(sub_a_immediate_disassembly.opcode_info.cycles, 2);
-        assert_eq!(sub_a_immediate_disassembly.operands.len(), 2);
-        assert_eq!(sub_a_immediate_disassembly.operands[0].operand_type, OperandType::AccumulatorA);
-        assert_eq!(sub_a_immediate_disassembly.operands[1].operand_type, OperandType::Immediate8);
-        assert_eq!(sub_a_immediate_disassembly.operands[1].operand_value, Some(0x5));
+        assert_ne!(sub_a_immediate_disassembly.operands, None);
+        sub_a_operands = sub_a_immediate_disassembly.operands.unwrap();
+        assert_eq!(sub_a_operands.len(), 2);
+        assert_eq!(sub_a_operands[0], OperandType::AccumulatorA);
+        assert_eq!(sub_a_operands[1], OperandType::Immediate8(0x5));
     }
 }
